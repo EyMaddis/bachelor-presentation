1e4ed391be21762057f80a4639cfc311bdb27662
amf <amf@informatik.uni-kiel.de>
2014-08-22 10:39:45 +0200

    changes example code in erlang

--- erlang/Beispiel.erl
+++ erlang/Beispiel.erl
@@ -2,7 +2,7 @@ start() -> pirate_talk(yarrh).
 
 pirate_talk(yarrh) -> io:fwrite("Yarrh!~n");
 
-pirate_talk(ney)   -> My = "My ", Mate = "Mate!"
+pirate_talk(ney)   -> My = "Me", Mate = "mate!"
                       io:fwrite("Ney!"),
                       io:fwrite("Don't to that!~n~n~p ~p~n", [My, 
                       Mate]),
