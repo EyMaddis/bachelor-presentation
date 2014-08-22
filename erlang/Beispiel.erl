6d618a5d8e95f1dcb25eb76b28debc428ca7184c
amf <amf@informatik.uni-kiel.de>
2014-08-22 10:25:29 +0200

    Adds code for erlang and changes the erlang sheets.

---
+++ erlang/Beispiel.erl
@@ -0,0 +1,9 @@
+start() -> pirate_talk(yarrh).
+
+pirate_talk(yarrh) -> io:fwrite("Yarrh!~n");
+
+pirate_talk(ney)   -> My = "My ", Mate = "Mate!"
+                      io:fwrite("Ney!"),
+                      io:fwrite("Don't to that!~n~n~p ~p~n", [My, 
+                      Mate]),
+                      io:fwrite("Never again, you hear me?~n").
